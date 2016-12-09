788dbced127aad11fd6d69e6dc59520c2b689fb5
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-12-09 02:40:09 +0200

    Security ...

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,53 @@
 Parse.Cloud.beforeSave("Barter", function (request, response) {
+    let keys = request.object.dirtyKeys();
+    for (let i = 0; i < keys.length; i++) {
+        if (typeof request.object.get(keys[i]) === 'string') {
+            request.object.set(keys[i], htmlentities(request.object.get(keys[i])));
+        } else if (typeof request.object.get(keys[i]) === '[object Array]') {
+            var ret = [];
+            let arr = request.object.get(keys[i]);
+            for (let j = 0; j < arr.length; j++) {
+                ret.push(htmlentities(arr[i]));
+            }
+            request.object.set(keys[i], ret);
+
+        } else if (keys[i] == 'barterRequests') {
+            var ret = [];
+            let obj = request.object.get(keys[i]);
+            let arr = obj.milestones;
+            for (let j = 0; j < arr.length; j++) {
+                ret.push(htmlentities(arr[i]));
+            }
+            obj.milestones = arr;
+            request.object.set(keys[i], obj);
+        }
+    }
     if (request.object.isNew()) {
     } else {
+        if (!request.user || ((request.user.id != request.object.get('user').id || request.user.id != request.object.get('barterUpUser').id) && !(request.object.dirtyKeys == 1 && request.object.dirty('barterRequests')))) {
+            response.error("Not Authorized");
+            return;
+        }
+        if (request.object.dirty('barterUpUser') && request.original.get('barterUpUser')) {
+            response.error("Not Authorized");
+            return;
+        }
+        if (request.user.id != request.object.get('user').id && ((request.object.dirty('barterUpMilestones') && request.original.get('barterUpMilestones') || (request.object.dirty('barterUpRate') || request.object.dirty('barterUpReview') || request.object.dirty('barterUpFinalPic') || request.object.dirty('barterUpDeadline'))))) {
+            response.error("Not Authorized");
+            return;
+        }
+
+        let dirtyKeys = request.object.dirtyKeys();
+        var flag = false;
+        var allowed = ['barterUpMilestones', 'barterUpRate', 'barterUpReview', 'barterUpFinalPic', 'barterUpDeadline'];
+        for (let i = 0; i < dirtyKeys.length; i++) {
+            if (allowed.indexOf(dirtyKeys[i]) == -1)
+                flag = true;
+        }
+        if (request.user.id != request.object.get('barterUpUser').id && flag) {
+            response.error("Not Authorized");
+            return;
+        }
         if (request.object.dirty('offerRate') && !request.original.get('offerRate')) {
             query = new Parse.Query(Parse.User);
             query.get(request.object.get("barterUpUser").id, {
@@ -100,4 +147,37 @@ function createNotification(user, event, creator, objectId) {
             console.error("Got an error " + error.code + " : " + error.message);
         }
     });
+}
+
+
+function htmlentities(string, quoteStyle, charset, doubleEncode) {
+    var getHtmlTranslationTable = require('../strings/get_html_translation_table')
+    var hashMap = getHtmlTranslationTable('HTML_ENTITIES', quoteStyle)
+
+    string = string === null ? '' : string + ''
+
+    if (!hashMap) {
+        return false
+    }
+
+    if (quoteStyle && quoteStyle === 'ENT_QUOTES') {
+        hashMap["'"] = '&#039;'
+    }
+
+    doubleEncode = doubleEncode === null || !!doubleEncode
+
+    var regex = new RegExp('&(?:#\\d+|#x[\\da-f]+|[a-zA-Z][\\da-z]*);|[' +
+        Object.keys(hashMap)
+            .join('')
+            // replace regexp special chars
+            .replace(/([()[\]{}\-.*+?^$|\/\\])/g, '\\$1') + ']',
+        'g')
+
+    return string.replace(regex, function (ent) {
+        if (ent.length > 1) {
+            return doubleEncode ? hashMap['&'] + ent.substr(1) : ent
+        }
+
+        return hashMap[ent];
+    })
 }
