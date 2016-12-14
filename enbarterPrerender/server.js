02d58cba965829112713efb9ae3d21ab21b266cd
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-12-14 21:31:42 +0200

    nginx,prerender ...

--- enbarterPrerender/server.js
+++ enbarterPrerender/server.js
@@ -1,27 +1,13 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
-process.env.ALLOWED_DOMAINS = 'www.enbarter.com,enbarter.com,github.com,localhost,ahmedengu.github.io';
-process.env.CACHE_ROOT_DIR = 'cache';
+process.env.ALLOWED_DOMAINS = 'www.enbarter.com,enbarter.com,enbarterdev.ml,www.enbarterdev.ml';
+process.env.CACHE_ROOT_DIR = '/srv/enbarterPrerender/cache';
 process.env.CACHE_LIVE_TIME = 10000;
 
 var server = prerender({
     workers: 4,
     iterations: 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    // accessLog: {
-    //     // Check out the file-stream-rotator docs for parameters
-    //     fileStreamRotator: {
-    //         filename: 'logs/access-%DATE%.log',
-    //         frequency: 'daily',
-    //         date_format: 'YYYY-MM-DD',
-    //         verbose: false
-    //     },
-    //
-    //     // Check out the morgan docs for the available formats
-    //     morgan: {
-    //         format: 'combined'
-    //     }
-    // },
+    phantomBasePort: 12300,
     cookiesEnabled: true,
     logRequests: true,
     pageDoneCheckTimeout: 300,
@@ -37,14 +23,8 @@ var server = prerender({
 server.use(require('prerender-compressed-file-cache'));
 
 server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.basicAuth());
 server.use(prerender.whitelist());
-//server.use(prerender.blacklist());
 server.use(prerender.logger());
-//server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
-// server.use(require('prerender-access-log'));
 
 server.start();
