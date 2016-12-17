8b37d0a1577bddc805e13e1f5e0cf83627d3d20a
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-12-17 03:48:15 +0200

    password reset working

--- enbarterPrerender/server.js
+++ enbarterPrerender/server.js
@@ -2,7 +2,7 @@
 var prerender = require('./lib');
 process.env.ALLOWED_DOMAINS = 'www.enbarter.com,enbarter.com,enbarterdev.ml,www.enbarterdev.ml';
 process.env.CACHE_MAXSIZE = 10000;
-
+process.env.CACHE_ROOT_DIR = __dirname + '/cache';
 var server = prerender({
     workers: 4,
     iterations: 10,
@@ -18,11 +18,25 @@ var server = prerender({
     evaluateJavascriptCheckTimeout: 300
 });
 
+var cache_config = {
+    /**
+     * Example of custom path builder
+     * @param key String the url of the resource being processed
+     * @return path String folder path like '../', 'f1/f2/f3/.../'
+     */
+    pathBuilder: function (key) {
+        var now = new Date();
+        return process.env.CACHE_ROOT_DIR + '/' + key;
+    },
+    // Custom file name
+    fileName: '.cache'
+}
+server.use(require('prerender-compressed-file-cache')(cache_config));
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.whitelist());
 server.use(prerender.logger());
 server.use(prerender.httpHeaders());
-server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.removeScriptTags());
 
 server.start();
--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 var raven = require('raven');
 var client = new raven.Client('https://22c41b4449c04f2f9678babd3400566c:db3b5311623146389b2afe0e37340d95@sentry.io/118691');
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
 var app = express();
-app.use('/public', express.static(path.join(__dirname, '/public')));
 
 var api = new ParseServer({
     appName: 'Enbarter',
-    publicServerURL: 'http://enbarter.com',
+    publicServerURL: 'http://api.enbarterdev.ml/v1',
     databaseURI: 'mongodb://enbarterUser:1d9bd5d441415fc6556acb447b97903f1623d16fd9d56fe@178.62.247.181:27017/enbarterDB',
     cloud: __dirname + '/cloud/main.js',
     appId: 'EnbarterApp',
     javascriptKey: '28e0691b32ab',
     masterKey: 'fb4b98ea158cbbdd32c366682f280533d89374a2fa8908186b4478ff295b96f77096f54eabc9a61b956237d817fb04ea6498c73c4cd9ec14e1ade7cc81136b0',
-    serverURL: 'http://178.62.247.181:1337/v1',
+    serverURL: 'http://api.enbarterdev.ml/v1',
     liveQuery: {
-        classNames: ["Barter", "Chat", "Notification"]
+        classNames: ["BarterDashboard", "Chat", "Notification"]
     },
     websocketTimeout: 10 * 1000,
     cacheTimeout: 60 * 600 * 1000,
     logLevel: 'VERBOSE',
     revokeSessionOnPasswordReset: true,
+    allowClientClassCreation: false,
+    enableAnonymousUsers: false,
+    facebookAppIds: ['1394780183887567'],
+    sessionLength: 2592000,
+    verifyUserEmails: true,
+    emailVerifyTokenValidityDuration: 24 * 60 * 60,
+    passwordPolicy: {
+        validatorPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+        doNotAllowUsername: true,
+        maxPasswordAge: 1000,
+        maxPasswordHistory: 5,
+        resetTokenValidityDuration: 24 * 60 * 60
+    },
+    auth: {
+        facebook: {
+            appIds: "1394780183887567"
+        }
+    },
     accountLockout: {
         duration: 5,
         threshold: 3
-    }
-    ,
+    },
     filesAdapter: {
         module: "parse-server-fs-adapter",
         options: {
-            filesSubDirectory: __dirname +"/files"
+            filesSubDirectory: ""
         }
     },
     emailAdapter: {
@@ -41,20 +57,17 @@ var api = new ParseServer({
             fromAddress: 'ahmedengu@enbarterdev.ml',
             user: 'ahmedengu@enbarterdev.ml',
             password: '123456789',
-            host: 'mail.enbarterdev.ml',
-            isSSL: false, //True or false if you are using ssl
-            port: 25, //SSL port or another port
+            host: 'enbarterdev.ml',
+            isSSL: true, //True or false if you are using ssl
+            port: 465, //SSL port or another port
             name: 'enbarterdev.ml', //  optional, used for identifying to the server
             //Somtimes the user email is not in the 'email' field, the email is search first in
             //email field, then in username field, if you have the user email in another field
             //You can specify here
             emailField: 'email',
             templates: {
-                //This template is used only for reset password email
                 resetPassword: {
-                    //Path to your template
                     template: __dirname + '/views/email/reset-password',
-                    //Subject for this email
                     subject: 'Reset your password'
                 }
             }
@@ -80,6 +93,3 @@ httpServer.listen(port, function () {
 });
 
 ParseServer.createLiveQueryServer(httpServer);
-
-
-// parse-dashboard --appId EnbarterApp --masterKey fb4b98ea158cbbdd32c366682f280533d89374a2fa8908186b4478ff295b96f77096f54eabc9a61b956237d817fb04ea6498c73c4cd9ec14e1ade7cc81136b0 --serverURL "http://api.enbarterdev.ml/v1"
